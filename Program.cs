d4ddb8e2941c84b685ed9dc791b91d50b1f6a720
hetyang <hakongo886@gmail.com>
2026-04-16 19:37:47 +0300

    Clean project and fix gitignore

--- Program.cs
+++ Program.cs
@@ -69,8 +69,8 @@ app.MapGet("/api/classify", async(string name, IHttpClientFactory httpClientFact
     };
     return Results.Ok(result);
 });
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-app.Run($"http://0.0.0.0:{port}");
+
+app.Run();
 public class GenderizeResponse
 {
     public int count{get; set;}
